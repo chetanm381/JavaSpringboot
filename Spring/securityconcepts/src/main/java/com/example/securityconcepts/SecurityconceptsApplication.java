96e377767d99597c9432beac01187b1e78007784
chetanm381 <60662707+chetanm381@users.noreply.github.com>
2025-05-20 10:56:59 +0000

    Commit Spring Project

---
+++ Spring/securityconcepts/src/main/java/com/example/securityconcepts/SecurityconceptsApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityconceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityconceptsApplication.class, args);
+	}
+
+}
---
+++ Spring/securityconcepts/src/test/java/com/example/securityconcepts/SecurityconceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityconceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
